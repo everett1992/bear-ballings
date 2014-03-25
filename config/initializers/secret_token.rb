1bb6324e65d04eecf409424fdb3a2290179adb45
Caleb Everett <everett.caleb@gmail.com>
2014-03-25 14:12:08 -0400

    Initial commit of rails skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BearBallings::Application.config.secret_key_base = '2bb77b2f11204213454c43713dbe35857721176aea4b5d09cde10b4c1858c4d1dfda077f1c05d3306cbba696bdf4bef78a62ba2d8f6f56871d0f4b8e3a513e58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BearBallings::Application.config.session_store :cookie_store, key: '_bear-ballings_session'
