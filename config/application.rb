f6b3faf295a5b90329a11569b387cb5e7559d4fc
Caleb Everett <everett.caleb@gmail.com>
2014-03-29 18:30:07 -0400

    Silence il8n errors.

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ Bundler.require(:default, Rails.env)
 
 module BearBallings
   class Application < Rails::Application
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
