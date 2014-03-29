4564088258ed4eb4487fbebc7c5ddf133f8b02de
Caleb Everett <everett.caleb@gmail.com>
2014-03-29 15:14:46 -0400

    Add department query to course index

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,5 +1,14 @@
 class Api::CoursesController < ApplicationController
   def index
-    @courses = Courses.limit(params[:limit]).offset(params[:offset])
+    @courses = Courses
+      .where(query_params)
+      .limit(params[:limit])
+      .offset(params[:offset])
+  end
+
+  private
+
+  def query_params
+    params.permit(:department)
   end
 end
