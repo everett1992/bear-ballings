c409c2e156b27dee55a3e7894bfdd9d3cd31e87d
Caleb Everett <everett.caleb@gmail.com>
2014-03-29 18:10:44 -0400

    I am a teapot

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ BearBallings::Application.routes.draw do
   namespace :api, defaults: {format: :json} do
     get 'departments'         => 'departments#index'
     get 'courses'             => 'courses#index'
+    get 'teapot'              => 'teapot#teapot'
     #get 'courses/:department' => 'courses#index'
   end
 end
