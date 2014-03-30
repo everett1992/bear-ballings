7fa8d85e4d25a7a0b878550c65a41632d4c97807
Caleb Everett <everett.caleb@gmail.com>
2014-03-30 18:05:00 -0400

    Add remove_course and add_course to bin and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
   include Mongoid::Document
   field :n, as: :name, type: String
-  embeds_many :bins
+  has_many :bins, dependent: :destroy
 
   # Validates that no bin has duplicate courses.
 
@@ -10,12 +10,14 @@ class User
   #
   # If no +bin+ is passed a new bin is created
   # The bin is returned
-  def add_course(course, bin=Bin.new)
-    bin.courses << course
+  def add_course(course, bin=bins.new)
+    bin.add_course course
     return bin
   end
 
-  def remove_course(course, bin)
+  def remove_course(course)
+    bin = bins.where(:course_ids => course).first
 
+    return bin.remove_course(course)
   end
 end
