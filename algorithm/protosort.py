a9ec09ee19c2fa3970c266ad1bfe3a6e71092559
Glen Oakley <goakley123@gmail.com>
2014-04-03 14:29:53 -0400

    Clarified flow of algorithm

--- algorithm/protosort.py
+++ algorithm/protosort.py
@@ -20,14 +20,10 @@ Class = collections.namedtuple("Class", ["id", "course_id", "seats"])
 def classsort(users, classes):
     """Place users into classes in-place.
 
-    Returns the users argument, which will be modified for enrollment.
+    Returns the users list, which will be modified for enrollment.
     """
     if is_complete(users):
         return users
-
-    print("Working...")
-    # print userclasslist
-    # print bucketlist
 
     # ---------------------------------------
     # try to place a student
@@ -40,19 +36,13 @@ def classsort(users, classes):
     max_credits = max([u.credits for u in users
                        if len(u.classes) == min_classes_len])
 
-    # pick one of the candidates to schedule
+    # schedule a user that meets the previous criteria
+    # and has not reached the credit limit
     user = random.choice([u for u in users
                           if u.credits == max_credits
-                          and len(u.classes) == min_classes_len])
+                          and len(u.classes) == min_classes_len
+                          and len(u.classes) + u.credits < MAXCREDITS])
 
-    # account for classes the user need not take by adding blank entries
-    # (if they have less than four classes this time around)
-    if user.credits > 12:
-        if user.credits + len(user.classes) >= MAXCREDITS:
-            user.classes.append([])
-            return classsort(users, classes)
-
-    # user still needs more classes
     open_class = None
     while user.buckets:
         bucket = user.buckets.pop()
@@ -60,27 +50,31 @@ def classsort(users, classes):
             # check if this class has been enrolled already for this user
             if course_id in [x[0] for x in user.classes]:
                 continue
+            # find an open class for this course
             open_classes = [c for c in classes
                             if c.course_id == course_id and c.seats > 0]
             if not open_classes:
                 continue
             open_class = open_classes[0]
+            # enroll the user in the class (store course_id and class_id)
             user.classes.append((course_id, open_class.id))
             break
         else:
+            # this bucket had no open course classes; move to the next bucket
             continue
         break
     else:
-        # could not add a user to their requested classes
+        # all requested course classes were full
         # pick a random class and enroll
         open_classes = [c for c in classes if c.seats > 0]
         if not open_classes:
             print("BAD NEWS: OUT OF CLASSES!")
             assert False
         open_class = random.choice(open_classes)
-        open_class.seats -= 1
         user.classes.append((open_class.course_id, open_class.id))
+    # ensure SOME class was picked
     assert open_class is not None
+    # recurse with the new state of the user being enrolled in a new class
     return classsort(users, [c if c != open_class else
                              Class(open_class.id,
                                    open_class.course_id,
