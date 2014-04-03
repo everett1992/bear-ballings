015a9d0ec84b3ea160dea0ca4af6c12b45ea97f1
Eric Palace <epalace510@gmail.com>
2014-04-03 00:36:10 -0400

    Added API endpoint to add new course to new bin at end of bins

--- app/controllers/api/users/courses_controller.rb
+++ app/controllers/api/users/courses_controller.rb
@@ -2,4 +2,18 @@ class Api::Users::CoursesController < Api::Users::UserController
   def index
     @bins = @user.bins
   end
+  
+  def create
+    if params[:_id].blank?
+      render json: {error: 'Course _id cannot be nil'}, status: :unprocessable_entity
+    else 
+      course = Course.where(_id: params[:_id]).first
+      if course.nil?
+        render json: {error: 'No course matching the _id provided was found'}, status: :not_found
+      else
+        @bin = @user.add_course(course)
+        render json: :success
+      end
+    end
+  end
 end
--- test/controllers/api/users/courses_controller_test.rb
+++ test/controllers/api/users/courses_controller_test.rb
@@ -1,15 +1,70 @@
 require 'test_helper'
 
 class Api::Users::CoursesControllerTest < ActionController::TestCase
-  test "Requires login redirects to login" do
+  #--Index Tests
+  test "Should redirect unlogged in users to login page" do
     logout
     get :index, {format: :json}
     assert_redirected_to login_form_path
   end
 
-  test "Requires login redirects to user show" do
+  test "Should redirect logged in user to user show" do
     login User.first
     get :index, {format: :json}
     assert_template :index
+  end
+
+  #--Adding Courses Tests
+  test "Should fail when no course _id is provided" do
+    login User.first
+    post :create, {format: :json}
+    assert_response :unprocessable_entity
+    assert_equal "Course _id cannot be nil", JSON.parse(response.body)['error']
+  end
+
+  test "Should fail when no course matches provided _id" do
+    login User.first
+    post :create, {format: :json, _id: "THIS WILL NEVER BE A COURSE ID. THAT'S NOT HOW THOSE WORK. THIS SHOULD BE SAFE BY NOW"}
+    assert_response :not_found
+    assert_equal "No course matching the _id provided was found", JSON.parse(response.body)['error']
+  end
+
+  test "Default case should create new bin and adds course" do
+    user = User.first
+    course = Course.first
+    login user
+    user.bins.delete_all
+    assert_difference('user.bins.count', 1) do
+      post :create, {format: :json, _id: course._id}
+      assert assigns(:bin).courses.include? course
+    end
+  end
+
+  test "Default case should add new bin to the end of the bins" do
+    user = User.first
+    course = Course.first
+    new_course = Course.last
+    login user
+    user.bins.delete_all
+    user.add_course(course)
+    post :create, {format: :json, _id: new_course._id}
+    assert_equal(user.reload.bins.last, assigns(:bin))
+  end
+
+  #--Routing Tests
+  test "Should route api/user/courses to api/user/courses#index" do
+    assert_routing({method: 'get', path: 'api/user/courses'}, {
+        format: :json,
+        action: 'index',
+        controller: 'api/users/courses'
+    })
+  end
+
+  test "Should route POST api/user/courses to api/user/courses#create" do
+    assert_routing({method: 'post', path: 'api/user/courses'}, {
+        format: :json,
+        action: 'create',
+        controller: 'api/users/courses'
+    })
   end
 end
