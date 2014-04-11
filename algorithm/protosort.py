6feeddc5db7eabc148598508d4c0a809ab3c3cd3
derricp1 <derricp1@apps.tcnj.edu>
2014-04-10 23:12:44 -0400

    Protosort has time functionality

--- algorithm/protosort.py
+++ algorithm/protosort.py
@@ -14,7 +14,7 @@ MAXCREDITS = 16
 
 User = collections.namedtuple("User", ["buckets", "classes", "credits"])
 
-Class = collections.namedtuple("Class", ["id", "course_id", "seats"])
+Class = collections.namedtuple("Class", ["id", "course_id", "seats", "day", "start_time", "end_time"])
 
 
 def classsort(users, classes):
@@ -52,12 +52,13 @@ def classsort(users, classes):
                 continue
             # find an open class for this course
             open_classes = [c for c in classes
-                            if c.course_id == course_id and c.seats > 0]
+                            if c.course_id == course_id and c.seats > 0 and (t_conflict(user.classes, c) == False)]
             if not open_classes:
                 continue
             open_class = open_classes[0]
+            
             # enroll the user in the class (store course_id and class_id)
-            user.classes.append((course_id, open_class.id))
+            user.classes.append((course_id, open_class.id, open_class.day, open_class.start_time, open_class.end_time))
             break
         else:
             # this bucket had no open course classes; move to the next bucket
@@ -66,19 +67,22 @@ def classsort(users, classes):
     else:
         # all requested course classes were full
         # pick a random class and enroll
-        open_classes = [c for c in classes if c.seats > 0]
+        open_classes = [c for c in classes if c.seats > 0 and (t_conflict(user.classes, c) == False)]
         if not open_classes:
             print("BAD NEWS: OUT OF CLASSES!")
             assert False
         open_class = random.choice(open_classes)
-        user.classes.append((open_class.course_id, open_class.id))
+        user.classes.append((open_class.course_id, open_class.id, open_class.day, open_class.start_time, open_class.end_time))
     # ensure SOME class was picked
     assert open_class is not None
     # recurse with the new state of the user being enrolled in a new class
     return classsort(users, [c if c != open_class else
                              Class(open_class.id,
                                    open_class.course_id,
-                                   open_class.seats - 1)
+                                   open_class.seats - 1,
+                                   open_class.day,
+                                   open_class.start_time,
+                                   open_class.end_time)
                              for c in classes])
 
 
@@ -88,19 +92,34 @@ def is_complete(users):
                 len(u.classes) + u.credits >= MAXCREDITS
                 for u in users])
 
+def t_conflict(user_classes, course):
+    days = [c[2] for c in user_classes] #list of days
+    if (course.day in days) == False:
+        return False
+    else:
+        c_starts = [c[3] for c in user_classes if c[2] == course.day]    
+        c_ends = [c[4] for c in user_classes if c[2] == course.day]
+        #will match
+        
+        for con in range(len(c_starts)):
+            if ((course.start_time >= c_starts[con] and course.start_time <= c_ends[con]) or (course.end_time >= c_starts[con] and course.end_time <= c_ends[con]) or (course.start_time <= c_starts[con] and course.end_time >= c_ends[con])):
+                return True
+            
+        return False
 
-SAMPLE_USERS = [User([[1, 2], [3, 4], [5, 6], [7, 8]], [], 12),
-                User([[1, 4, 6], [2, 5], [7, 9], [4, 3]], [], 3),
-                User([[3, 4], [5, 6], [1, 10], [7, 8, 9]], [], 5)]
-SAMPLE_CLASSES = [Class(0, 1, 20),
-                  Class(1, 2, 10),
-                  Class(2, 3, 10),
-                  Class(3, 4, 15),
-                  Class(4, 5, 15),
-                  Class(5, 6, 20),
-                  Class(6, 7, 20),
-                  Class(7, 8, 10),
-                  Class(8, 9, 5),
-                  Class(9, 10, 100)]
+
+SAMPLE_USERS = [User([["CSC470", "CSC460"], ["CSC310", "CSC320"], ["CSC330", "CSC360"], ["WGS220", "WGS320"]], [], 12),
+                User([["CSC470", "CSC320", "CSC360"], ["CSC460", "CSC330"], ["WGS220", "PHY120"], ["CSC320", "CSC310"]], [], 3),
+                User([["CSC310", "CSC320"], ["CSC330", "CSC360"], ["CSC470", "PHY220"], ["WGS220", "WGS320", "PHY120"]], [], 5)]
+SAMPLE_CLASSES = [Class(0, "CSC470", 20, 1, 1000, 1120),
+                  Class(1, "CSC460", 10, 1, 1000, 1130),
+                  Class(2, "CSC310", 10, 1, 900, 1500),
+                  Class(3, "CSC320", 15, 2, 900, 1030),
+                  Class(4, "CSC330", 15, 2, 1030, 1200),
+                  Class(5, "CSC360", 20, 3, 1100, 1230),
+                  Class(6, "WGS220", 20, 3, 2000, 2100),
+                  Class(7, "WGS320", 10, 3, 1400, 1530),
+                  Class(8, "PHY120", 5, 4, 1200, 1330),
+                  Class(9, "PHY220", 100, 5, 830, 1000)]
 RESULT = classsort(SAMPLE_USERS, SAMPLE_CLASSES)
 print([r.classes for r in RESULT])
