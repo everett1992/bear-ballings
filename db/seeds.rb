101f4eb9dae1312dc276d3b9843f400199ca2caa
derricp1 <derricp1@apps.tcnj.edu>
2014-04-26 20:03:30 -0400

    Expanded comments further

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#Bear-Ballings:  Patrick D'Errico, Glen Oakley, Caleb Everett, Eric Palace
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,6 +8,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
+
+#Creates initial user and course data.
 Rake::Task['courses:parse'].invoke
 usernames = ['palacee1', 'everetc1']
 usernames.each do |name|
